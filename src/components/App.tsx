84483ef370d6ead52f6bf185027e35e5dacd5ad4
JHSeo <qnfqnfqnf@gmail.com>
2020-11-15 16:28:55 +0900

    add react-helmet-async
    - HelmetTemplate.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,15 +3,18 @@ import { Provider } from "react-redux";
 import store from "store";
 import AppRouter from "components/AppRouter";
 import { ThemeProvider } from "styled-components";
-import { lightTheme, darkTheme } from "styles/theme";
+import { theme } from "styles/theme";
 import { GlobalStyle } from "styles/globalStyles";
+import HelmetTemplate from "./base/HelmetTemplate";
 
 function App() {
   return (
     <Provider store={store}>
-      <ThemeProvider theme={lightTheme}>
-        <GlobalStyle />
-        <AppRouter />
+      <ThemeProvider theme={theme.lightTheme}>
+        <HelmetTemplate>
+          <GlobalStyle />
+          <AppRouter />
+        </HelmetTemplate>
       </ThemeProvider>
     </Provider>
   );
