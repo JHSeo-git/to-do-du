fae0a623dcd5d120ea6f8409095c8cf87228f7df
JHSeo-git <qnfqnfqnf@gmail.com>
2020-11-20 16:05:32 +0900

    add firestore fb

--- src/fBase.ts
+++ src/fBase.ts
@@ -1,5 +1,6 @@
 import firebase from "firebase/app";
 import "firebase/auth";
+import "firebase/firestore";
 
 const {
   REACT_APP_APIKEY,
@@ -29,4 +30,5 @@ firebase.initializeApp(firebaseConfig);
 // Initialize Firebase
 export const firebaseInstance = firebase;
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
 //  firebase.analytics();
