6ce65679fad102ecce218f64c71c460a01830bc6
JHSeo <qnfqnfqnf@gmail.com>
2020-12-06 19:17:11 +0900

    add get collection with condition user

--- src/components/todo/Todos.tsx
+++ src/components/todo/Todos.tsx
@@ -4,6 +4,7 @@ import useTodoState from "lib/hooks/redux/todos/useTodoState";
 import Todo from "components/todo/Todo";
 import NewTodo from "components/todo/NewTodo";
 import useSyncTodos from "lib/hooks/redux/todos/useSyncTodos";
+import useUserState from "lib/hooks/redux/user/useUserState";
 
 const TodosWrapper = styled.div`
   background: ${(props) => props.theme.whiteColor};
@@ -19,26 +20,29 @@ const TodoItems = styled.ul`
 const TodoItem = styled.li``;
 
 const Todos = () => {
+  const userState = useUserState();
   const todoState = useTodoState();
   const syncTodos = useSyncTodos();
 
   // TODO: refactoring
   useEffect(() => {
-    syncTodos();
-  }, [syncTodos]);
+    if (!userState.user || !userState.user.uid) return;
+    syncTodos(userState.user.uid);
+  }, [userState.user, syncTodos]);
 
   return (
     <TodosWrapper>
       <NewTodo />
       <TodoItems>
-        {todoState.todos?.map((todo) => (
-          <TodoItem key={todo.id}>
-            <Todo
-              {...todo}
-              isSelected={todoState.selectedTodo?.id === todo.id}
-            />
-          </TodoItem>
-        ))}
+        {todoState.todos &&
+          todoState.todos.map((todo) => (
+            <TodoItem key={todo.id}>
+              <Todo
+                {...todo}
+                isSelected={todoState.selectedTodo?.id === todo.id}
+              />
+            </TodoItem>
+          ))}
       </TodoItems>
     </TodosWrapper>
   );
