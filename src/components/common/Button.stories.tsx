7e4336b266e5f5b20717184ffb0b6a1c033d9a57
JHSeo <qnfqnfqnf@gmail.com>
2020-12-24 22:49:42 +0900

    move and default test

--- src/components/common/Button.stories.tsx
+++ src/components/common/Button.stories.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 // also exported from '@storybook/react' if you can deal with breaking changes in 6.1
 import { Story, Meta } from '@storybook/react/types-6-0';
 
+import 'styled-components';
+
 import { Button, ButtonProps } from './Button';
 
 export default {
@@ -12,13 +14,14 @@ export default {
   },
 } as Meta;
 
-// const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
 
-// export const Primary = Template.bind({});
-// Primary.args = {
-//   primary: true,
-//   label: 'Button',
-// };
+export const Default = Template.bind({});
+Default.args = {
+  provider: undefined,
+  isLoading: false,
+  children: 'Button',
+};
 
 // export const Secondary = Template.bind({});
 // Secondary.args = {
