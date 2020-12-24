4b60e62d795552ec13245b01711fd59c173b6cf4
JHSeo <qnfqnfqnf@gmail.com>
2020-12-24 22:49:27 +0900

    preview wrap providers
    - styled-component
    - redux
    - helmet-provider

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,26 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { HelmetProvider } from 'react-helmet-async';
+import { ThemeProvider } from 'styled-components';
+import HelmetGlobal from 'components/base/HelmetGlobal';
+import store from 'store';
+import { GlobalStyle } from 'styles/globalStyles';
+import { theme } from 'styles/theme';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
+
+export const decorators = [
+  (Story) => (
+    <Provider store={store}>
+      <ThemeProvider theme={theme.lightTheme}>
+        <HelmetProvider>
+          <HelmetGlobal />
+          <GlobalStyle />
+          <Story />
+        </HelmetProvider>
+      </ThemeProvider>
+    </Provider>
+  ),
+];
